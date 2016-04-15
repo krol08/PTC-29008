96841968be6ec65c93f15aa16f606bde90d94f0f
krol08 <karolrocha0802@gmail.com>
2016-04-15 14:06:32 -0300

    Enquadramento e desenquadramento de quadros.
    Versão do projeto enquadramento de quadros. Não possui detecção de erro e nem retransmissão de quadros.

---
+++ enquadramento.h
@@ -0,0 +1,56 @@
+#ifndef ENQUADRAMENTO_H
+#define ENQUADRAMENTO
+
+#include "serial.h"
+#include <iostream>
+#include <sstream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+
+//n_bytes - tamanho do quadro, alimentado pela máquina de estados.
+using namespace std;
+
+
+class Enquadramento {
+ public:
+
+
+  Enquadramento(Serial * dev, int bytes_min, int bytes_max);
+  ~Enquadramento();
+
+  // envia o quadro apontado por buffer
+  // o tamanho do quadro é dado por bytes
+  void enviar(void * buffer, int bytes);
+
+  // espera e recebe um quadro, armazenando-o em buffer
+  // retorna o tamanho do quadro recebido
+  int receber(void * buffer);
+
+ private:
+
+  enum Estados {
+    oscioso, start, recepcao, escape
+  };
+
+  int _min_bytes, _max_bytes;
+  Serial * porta;
+
+  // bytes recebidos pela MEF até o momento
+  int n_bytes;
+
+  // estado atual da MEF
+  Estados _estado_atual;
+
+  // aqui se implementa a máquina de estados de recepção
+  // retorna true se reconheceu um quadro completo
+  // bytes de dados são gravados no buffer
+  bool mef_trata_byte(char byte, char * buffer);
+
+};
+
+#endif
