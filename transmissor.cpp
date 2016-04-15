386352a5d0d4b2675eb6f220ea738afb0755bbbf
krol08 <karolrocha0802@gmail.com>
2016-04-15 14:07:17 -0300

    Transmissor de quadros.

---
+++ transmissor.cpp
@@ -0,0 +1,53 @@
+/*
+ * transmissor.cpp
+ *
+ *  Created on: 15 de abr de 2016
+ *      Author: root
+ */
+
+
+/*
+ * main.cc
+ *
+ *  Created on: 15 de abr de 2016
+ *      Author: root
+ */
+
+#include "serial.h"
+#include "enquadramento.h"
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+using namespace std;
+
+int main() {
+  Serial rf("/dev/ttyUSB0", B9600);
+
+  Enquadramento fra(&rf, 8,256);
+
+
+ char * msg = "Um teste~~] do trans~m}i]ssor via ~serial!!!";
+ //char * msg = "Um teste~~] d"; 
+ char buffer[256];
+
+ // int n = rf.Write(msg, strlen(msg));
+  fra.enviar(msg,strlen(msg));
+  //cout << "Enviou " << n << " bytes" << endl;
+
+
+
+}
+
+
+
+
+
+
+
+
